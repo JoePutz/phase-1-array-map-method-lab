e3cdf3410d06136108a846643d607323df00dbea
Joseph <josephputz89@gmail.com>
2022-05-18 08:22:00 -0500

    commit tree bb9eb7d1d88e41dd64b6030ca3d89a5a382fc94e

--- index.js
+++ index.js
@@ -11,6 +11,35 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials
+// }
 const titleCased = () => {
-  return tutorials
+  function capitalize(sentence) {
+    const words = sentence.split(' ');
+    const result = words.map(e => e[0].toUpperCase() + e.slice(1));
+    const newSentence = result.join(' ');
+    return newSentence;
+  }
+  return tutorials.map(capitalize);
 }
+
+
+// const titleCased = () => {
+//   const words = tutorials.split(" ");
+//   words.map(first => )
+//   const capTutorial = tutorials.map(first => first[0].toUpperCase() + first.substring(1)).join(" ")
+//     return capTutorial;
+// }
+
+// const titleCased = () => {
+//   tutorials.map(function(word) {
+
+//   let words = word.split(" "); 
+//   return words.map(function(word){
+//   return word.charAt(0).toUpperCase()+ word.substring(1)
+//   }).join(" "); ///return back to string 
+
+  
+//   }
+// };
